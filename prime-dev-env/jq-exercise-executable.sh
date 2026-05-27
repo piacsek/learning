77cb3a04834804132754b81b83c10e8d7e8a5a4c
Felipe Moraes Piacsek <felipe@piacsek.me>
2026-05-27 15:20:56 -0700

    real life jq examples

--- prime-dev-env/jq-exercise-executable.sh
+++ prime-dev-env/jq-exercise-executable.sh
@@ -1,3 +1,8 @@
 cat ./jq-exercise | jq 'map(select(.type == "foo"))'
 cat ./jq-exercise | jq 'map(select(.type == "foo") | .valueSum = (.values | add))'
 
+# Real life!
+cat /Users/piacsek/scratch.nvim/26-05-27_14-44-52.json | jq '[.forms[].content.headerView] | unique'
+cat /Users/piacsek/scratch.nvim/26-05-27_14-44-52.json | jq '.forms[].content | keys'
+cat /Users/piacsek/scratch.nvim/26-05-27_14-44-52.json | jq '.forms[].content | select(.title? and .title != "").title'
+
